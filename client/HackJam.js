5ea3ac494e3c68e8b4bc71e3ce39f0c1bd90acbf
Luke Sheard <me@lukesheard.com>
2015-03-01 14:23:14 -0800

    form added

--- client/HackJam.js
+++ client/HackJam.js
@@ -5,74 +5,29 @@ if(Meteor.isClient){
       tasks: function(){
          return Tasks.find(
             {},
-            {sort: {checked: 1, completedAt: 1, createdAt: -1}}
+            {sort: {createdAt: -1}}
          );
       }
    });
 
    Template.body.events({
       "submit .new-task": function(event) {
-         var text = event.target.text.value;
-         var date = new Date();
+         var course = event.target.course.value;
+         var title = event.target.title.value;
+         var date = event.target.date.value;
+         var notes = event.target.notes.value;
+         var createdAt = new Date();
+         
          Tasks.insert({
-            text: text,
-            createdAt: date,
-            day: date.getDate(),
-            month: date.getMonth() + 1,
-            year: date.getFullYear(),
-            owner: Meteor.userId(),
-            username: Meteor.user().username
+            course: course,
+            title: title,
+            date: date,
+            notes: notes,
+            createdAt: createdAt,
          });
 
          event.target.text.value="";
          return false;
       }
-   });
-
-   Template.task.helpers({
-      userCheck: function(){
-         if (Meteor.userId() == this.owner){
-            return true;
-         }
-         else{
-            return false;
-         }
-
-      }
-   });
-
-   Template.body.events({
-      "click .item": function(event){
-         Tasks.update(
-            this._id,
-            {
-               $set: {
-                  completedAt: new Date(),
-                  checked: !this.checked
-               }
-            }
-         );
-      }, 
-
-      "click .checked": function(event){
-         Tasks.remove(this._id);
-      },
-
-      "click .undo": function(event){
-         Tasks.update(
-            this._id,
-            {
-               $set: {
-                  completedAt: null,
-                  checked: !this.checked
-               }
-            }
-         );
-      }
-
-   });
-
-   Accounts.ui.config({
-      passwordSignupFields: "USERNAME_ONLY"
    });
 }
