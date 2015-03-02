9d27878b4f60e4a2c7fa9fa62326056bbce59605
Navrit Bal <navritbal@gmail.com>
2015-03-01 22:01:59 -0800

    Last minute

--- client/bootstrap/login.js
+++ client/bootstrap/login.js
@@ -4,7 +4,11 @@ if(Meteor.isClient){
 		    event.preventDefault();
 		    var username = template.find('#login-username').value;
 		    var passwordVar = template.find('#login-password').value;
-		    Meteor.loginWithPassword(username, passwordVar);
+		    if (1){
+		    	Meteor.loginWithPassword(username, passwordVar);
+		    } else {
+		    	window.alert("Failed");
+			}
 		}
 	});
 
@@ -15,25 +19,28 @@ if(Meteor.isClient){
 		    var passwordVar = template.find('#register-password').value;
 		    var p1 = passwordVar;
 		    var p2 = template.find('#register-password-confirm').value;
-		    if ((username.length) > 2) {
-		    	if ((p1.length) > 6){
-			    	if(p1 === p2){
-				    	Accounts.createUser({
-				        	username: username,
-				        	password: passwordVar
-				    	});
-			    	} else {
-			    		window.alert("Passwords do NOT match. Try again!");
-			    	}	
-			    } else{
-			    	window.alert("Enter a password with more than 6 characters");
-			    }	
-		    } else {
-		    	window.alert("Enter a username with more than 2 characters");
+		    if (username /*exists already*/){
+		    	if ((username.length) > 2) {
+			    	if ((p1.length) > 6){
+				    	if(p1 === p2){
+					    	Accounts.createUser({
+					        	username: username,
+					        	password: passwordVar
+					    	});
+				    	} else {
+				    		window.alert("Passwords do NOT match. Try again!");
+				    	}	
+				    } else{
+				    	window.alert("Enter a password with more than 6 characters");
+				    }	
+			    } else {
+			    	window.alert("Enter a username with more than 2 characters");
+			    }
 		    }
+		    
 		    
 		    
 		}
 	});
 }
-/* Username > 4, Password > 6, shorten if statement, */
+/*username exists, failed sign in attempt */
